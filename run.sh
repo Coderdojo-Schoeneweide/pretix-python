f7068dfcb88f37ceb402465ed38843ea63adc627
Cirrous <christianraisch@gmail.com>
2025-12-05 18:23:43 +0100

    Changed events to show next 8 thursdays aswell

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 case "$1" in
 	r)
-		PYTHONPATH=pretix-python main.py
+		PYTHONPATH=pretix-python ./main.py
 		;;
 	t)
 		PYTHONPATH=pretix-python ipython --no-banner
 		;;
 	-h) 
 		echo "--------------------------"
-		echo "/ How to use this script \\"
+		echo "| How to use this script |"
 		echo "--------------------------"
 		echo ""
 		echo "r - lets you run the programm in normal mode"
