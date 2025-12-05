c402cea34c0f3a8712372f60cacc10f03ca836f4
Cirrous <christianraisch@gmail.com>
2025-12-05 23:08:45 +0100

    wrote install script to use "pretix" as a global command

--- run.sh
+++ run.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+PROJ="$(cd "$(dirname "$0")" && pwd)"
+
 case "$1" in
-	r)
-		PYTHONPATH=pretix-python ./main.py
-		;;
-	t)
-		PYTHONPATH=pretix-python ipython --no-banner
-		;;
-	-h) 
-		echo "--------------------------"
-		echo "| How to use this script |"
-		echo "--------------------------"
-		echo ""
-		echo "r - lets you run the programm in normal mode"
-		echo "t - lets you run the programm in test mode"
-		;;
-	*)
-		echo "invalid option: \"$1\" use -h for help"
-		;;
+    r)	
+		source "$PROJ/venv/bin/activate"
+        PYTHONPATH="$PROJ/pretix-python" python3 "$PROJ/main.py"
+        ;;
+    t)	
+		source "$PROJ/venv/bin/activate"
+        PYTHONPATH="$PROJ/pretix-python" ipython --no-banner
+        ;;
+    -h)
+        echo "--------------------------"
+        echo "| How to use this script |"
+        echo "--------------------------"
+        echo ""
+        echo "r - lets you run the programm in normal mode"
+        echo "t - lets you run the programm in test mode"
+        ;;
+    *)
+        echo "invalid option: \"$1\" use -h for help"
+        ;;
 esac
