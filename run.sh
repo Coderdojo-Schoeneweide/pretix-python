4f7f77bc3c7c6f9c4dc7d38e28065ebe3554b587
Cirrous <christianraisch@gmail.com>
2025-12-05 17:46:34 +0100

    Added -h to bash script and main.py out of the test folder

--- run.sh
+++ run.sh
@@ -2,12 +2,20 @@
 
 case "$1" in
 	r)
-		PYTHONPATH=pretix-python ./tests/main.py
+		PYTHONPATH=pretix-python main.py
 		;;
 	t)
 		PYTHONPATH=pretix-python ipython --no-banner
 		;;
+	-h) 
+		echo "--------------------------"
+		echo "/ How to use this script \\"
+		echo "--------------------------"
+		echo ""
+		echo "r - lets you run the programm in normal mode"
+		echo "t - lets you run the programm in test mode"
+		;;
 	*)
-		echo "invalid option: \"$1\""
+		echo "invalid option: \"$1\" use -h for help"
 		;;
 esac
